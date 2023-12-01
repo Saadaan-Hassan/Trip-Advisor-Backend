47500d62e3fb3941e3487365249a59b30baa443c
Saadaan Hassan <96711267+Saadaan-Hassan@users.noreply.github.com>
2023-12-01 20:51:14 +0500

    add cors configuration to connect to frontend

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-app.use(cors());
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Update this with your actual client's origin
+    credentials: true,
+  })
+);
 
 app.use("/api/v1/tripadvisor", api);
 app.get("/", (req, res) => {
